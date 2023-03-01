935c4874aa6333a3207c60bf8bafbbafa1ad7714
[Alexandr] <[afominyh242@gmail.com]>
2023-03-02 01:41:27 +0700

    Сделал так, чтобы прогноз погоды выводился аналогично с требованием задания.

--- app_main.py
+++ app_main.py
@@ -1,9 +1,15 @@
 import requests
 
-weather_forecast_location = 'Череповец'
-url_template = 'https://wttr.in/{}?mnTqu&lang=ru'
-ready_url = url_template.format(weather_forecast_location)
-response = requests.get(ready_url)
-response.raise_for_status()
 
-print(response.text.strip())
+def get_weather_forecast(loc):
+    """Функция, которая возвращает прогноз погоды если передать ей локацию"""
+    url_template = 'https://wttr.in/{}?mnTqu&lang=ru'
+    ready_url = url_template.format(loc)
+    response = requests.get(ready_url)
+    response.raise_for_status()
+    return response.text.strip()
+
+
+weather_forecast_locations = ['Лондон', 'Шереметьево', 'Череповец']
+for location in weather_forecast_locations:
+    print(get_weather_forecast(location))
